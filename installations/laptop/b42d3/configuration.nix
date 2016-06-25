51b2fe84c7cbcb4f4e1bbfbaf1646ef4fc09af5e
c74d <8573dd@gmail.com>
2016-06-25 11:29:47 +0000

    Properly merge b42d3 redshift config into union

--- installations/laptop/b42d3/configuration.nix
+++ installations/laptop/b42d3/configuration.nix
@@ -46,15 +46,6 @@
 
   services.redshift = {
     enable = !config.environment.noXlibs;
-    # City
-    latitude = "34";
-    longitude = "-118";
-    # Conf-US-CO-GE
-    #latitude = "39";
-    #longitude = "-105";
-    # Conf-US-NC-Gy
-    #latitude = "36";
-    #longitude = "-80";
   };
 
   services.xserver = {
--- sections/services/redshift/default.nix
+++ sections/services/redshift/default.nix
@@ -1,8 +1,15 @@
 { config, lib, pkgs, ... }: {
 
   services.redshift = {
+    # Home
     latitude = "34";
     longitude = "-118";
+    # Conf-US-CO-GE
+    #latitude = "39";
+    #longitude = "-105";
+    # Conf-US-NC-Gy
+    #latitude = "36";
+    #longitude = "-80";
     temperature = {
       night = 1850;
     };
