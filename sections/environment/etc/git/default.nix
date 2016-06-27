213704bed7ed4d8ba0b3e6ccbe6c1b3ca0e891d1
c74d <8573dd@gmail.com>
2016-06-27 19:03:21 +0000

    personal: Add Git aliases
    Add my Git aliases for personal configurations.

--- sections/environment/etc/git/default.nix
+++ sections/environment/etc/git/default.nix
@@ -25,6 +25,30 @@
     [rebase]
         missingCommitsCheck = error
         stat = yes
+  '' + lib.optionalString config.c74d-params.personal ''
+    [alias]
+        vc = commit --verbose
+        s = status
+        co = checkout
+        mwdiff = diff --minimal --word-diff
+        gl = log --graph
+        sl = log --decorate --source --format=short --encoding=UTF-8 --notes --show-signature --date=iso --graph
+        xl = log --decorate --source --format=fuller --encoding=UTF-8 --notes --show-signature --date=iso --graph
+        sxl = log --decorate --source --format=fuller --encoding=UTF-8 --notes --show-signature --date=iso --graph --stat --patch
+        pxl = log --decorate --source --format=fuller --encoding=UTF-8 --notes --show-signature --date=iso --graph --stat --patch
+        wpxl = log --decorate --source --format=fuller --encoding=UTF-8 --notes --show-signature --date=iso --graph --stat --patch --word-diff
+        ffmerge = merge --ff-only
+        noffmerge = merge --no-ff
+        ffpull = pull --ff-only
+        noffpull = pull --no-ff
+        ir = rebase --interactive
+        dry-merge = "!f() { grep -Fqm 1 'changed in both' <<< $(git merge-tree $(git merge-base $1 $2) $2 $1) && echo 'Merge conflict detected' || echo 'Merged cleanly'; }; f"
+        fgrep = grep --fixed-strings
+        egrep = grep --extended-regexp
+        pgrep = grep --perl-regexp
+        lsmergeconflicts = ls-files --unmerged
+        gca = gc --aggressive
+        xfsck = fsck --full --strict --unreachable --dangling
   '';
 
 }
