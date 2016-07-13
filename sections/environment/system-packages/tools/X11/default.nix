367b79d3e4f311ea0503da5ee5772790f7211fa5
c74d <8573dd@gmail.com>
2016-07-13 20:22:57 +0000

    Only install dmenu and i3status when i3 enabled

--- sections/environment/system-packages/tools/X11/default.nix
+++ sections/environment/system-packages/tools/X11/default.nix
@@ -1,9 +1,10 @@
 { config, lib, pkgs, ... }: {
 
   environment.systemPackages = lib.optionals config.services.xserver.enable (with pkgs; [
+    xclip
+  ] ++ lib.optionals config.c74d-params.i3.enable [
     dmenu
     i3status
-    xclip
   ]);
 
 }
