3e306bec5bf34e14238f82bf2606dfc5e25da2bc
c74d <8573dd@gmail.com>
2016-08-13 00:02:51 +0000

    X resources: set XTerm*charClass for URL selection
    Set the X resource `XTerm*charClass` for ease of selecting URLs in
    xterm.

--- sections/services/xserver/xresources/xterm.nix
+++ sections/services/xserver/xresources/xterm.nix
@@ -1,6 +1,12 @@
 { ... }: {
 
   services.xserver.xresources = {
+    # Set `charClass` for ease of selecting URLs. This value is sourced from
+    # <https://lukas.zapletalovi.com/2013/07/hidden-gems-of-xterm.html#triple-click>,
+    # albeit slightly changed to include the '#' character.
+    "XTerm*charClass" =
+      "33:48,35-47:48,58-59:48,61:48,63-64:48,95:48,126:48";
+
     "XTerm*termName" = "xterm-256color";
   };
 
