c2570a9cc9497e1c4006fc3eb1f4c9f869964f8e
c74d <8573dd@gmail.com>
2016-08-13 08:30:25 +0000

    vim-try-x: init at 1.0.0; add to system packages
    Add a `vim-try-x` package, which is a wrapper script (wholly contained
    in the derivation definition) around Vim, for starting Vim with an X
    server if one's available and without complaint if one's not.
    
    Furthermore, add this package to the set of system packages.

---
+++ pkgs/own/vim-try-x/default.nix
@@ -0,0 +1,52 @@
+{ stdenv, vim, writeScriptBin }:
+
+stdenv.mkDerivation rec {
+  name = "${scriptName}-${version}";
+  scriptName = "vim-try-x";
+  version = "1.0.0";
+  
+  src = ''
+    #!/bin/sh
+
+    g=
+
+    if [ -n "$DISPLAY" ]; then
+      g='-g'
+    fi
+
+    exec '${vim}/bin/vim' $g "$@"
+  '';
+
+  phases = [
+    "installPhase"
+    "fixupPhase"
+  ];
+
+  installPhase = ''
+    mkdir -p "$out/bin"
+    echo -E "$src" >> "$out/bin/$scriptName"
+    chmod +x "$out/bin/$scriptName"
+  '';
+
+  dontPatchELF = true;
+  dontStrip = true;
+
+  meta = {
+    description =
+      "A wrapper around Vim that has it connect to X if available";
+    longDescription = ''
+      A wrapper script around Vim that has it connect to an X server if the
+      `DISPLAY` environment variable is set, without emitting an error message
+      if the variable is unset.
+    '';
+    license = stdenv.lib.licenses.zlib;
+    platforms =
+      let
+        vim-platforms =
+          stdenv.lib.platforms.unix;
+      in
+        assert vim.meta ? platforms ->
+         vim.meta.platforms == vim-platforms;
+        vim-platforms;
+  };
+}
--- sections/environment/system-packages/applications/X11-only/default.nix
+++ sections/environment/system-packages/applications/X11-only/default.nix
@@ -6,6 +6,10 @@
     gimp
     inkscape
     nmap_graphical
+
+    (config.lib.c74d.call-pkg "own/vim-try-x" {
+      vim = config.programs.vim.package;
+    })
   ]);
 
 }
