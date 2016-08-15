b6f31e9346b2fa07db25362c021eb5f63822ad94
c74d <8573dd@gmail.com>
2016-08-15 03:24:44 +0000

    Personal: Set EDITOR to vim-try-x-nofork
    In personal mode, set the environment variable `EDITOR` to
    `vim-try-x-nofork` rather than `vim-try-x`.

--- sections/environment/variables/personal/default.nix
+++ sections/environment/variables/personal/default.nix
@@ -3,7 +3,7 @@
   environment.variables = lib.mkIf config.c74d-params.personal {
     EDITOR =
       if config.services.xserver.enable then
-        "vim-try-x"
+        "vim-try-x-nofork"
       else
         "vim";
 
