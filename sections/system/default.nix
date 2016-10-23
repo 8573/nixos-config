ef3a4ea35685f4fb816ef4263c7f02e8d2b9c2f2
c74d <8573dd@gmail.com>
2016-10-23 08:03:38 +0000

    system.autoUpgrade.dates: Don't assume PDT
    When attempting to set the option `system.autoUpgrade.dates` to 04:40
    local political time, don't assume that the local timezone is Pacific
    Daylight Time if the system timezone is set to UTC; instead, use the
    average timezone offset in the location record
    `c74d-params.location.target`.

--- sections/system/default.nix
+++ sections/system/default.nix
@@ -15,10 +15,31 @@
           .${config.c74d-params.installation-type}
         }";
       dates =
-        lib.mkIf
-          (assert config.time ? timeZone;
-            config.time.timeZone == "UTC")
-          "11:40";
+        let
+          tz-offset =
+            config.c74d-params.location.target.timezone-avg-offset;
+          hour =
+            assert tz-offset < 24;
+            assert tz-offset > -24;
+            4 - tz-offset;
+          hour' =
+            assert hour >= -24;
+            assert hour <= 47;
+            if hour < 0 then
+              hour + 24
+            else if hour >= 24 then
+              hour - 24
+            else
+              hour;
+          value =
+            assert hour' >= 0;
+            assert hour' < 24;
+            "${toString hour'}:40";
+        in
+          lib.mkIf
+            (assert config.time ? timeZone;
+              config.time.timeZone == "UTC")
+            value;
       flags = [
         "-I" "AUTO-BUILD-FLAG=/dev/null/AUTO-BUILD-FLAG"
       ];
