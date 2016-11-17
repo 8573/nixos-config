a91ad251b92f8331ac3e1400f09c5185481dee9f
c74d <8573dd@gmail.com>
2016-11-17 01:39:39 +0000

    Disable SANE to work around NixOS/nixpkgs#20489

--- sections/hardware/default.nix
+++ sections/hardware/default.nix
@@ -3,7 +3,7 @@
   hardware.enableAllFirmware = config.c74d-params.enable-all-firmware;
 
   hardware.sane = {
-    enable = config.c74d-params.installation-type != "server";
+    enable = config.c74d-params.installation-type != "server" && false;
     extraBackends = lib.mkIf config.hardware.sane.enable (with pkgs; [
       hplip
     ]);
