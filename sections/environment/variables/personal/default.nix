c3789b4cf6c4d2518b346f36f3dbbea096379c73
c74d <8573dd@gmail.com>
2016-11-25 09:30:45 +0000

    Personal: env vars: set VISUAL equal to EDITOR

--- sections/environment/variables/personal/default.nix
+++ sections/environment/variables/personal/default.nix
@@ -2,20 +2,24 @@
 
   c74d-pkgs = config.lib.c74d.pkgs;
 
+  visual-editor =
+    if config.services.xserver.enable then
+      "vim-try-x-nofork"
+    else
+      "vim";
+
 in {
 
   environment.variables = lib.mkIf config.c74d-params.personal {
-    EDITOR =
-      if config.services.xserver.enable then
-        "vim-try-x-nofork"
-      else
-        "vim";
+    EDITOR = visual-editor;
 
     PAGER = "vim-pager";
 
     MANPAGER = "vim-manpager";
 
     VIMPAGER_VIM = "${c74d-pkgs.c74d.vim-try-x}/bin/vim-try-x";
+
+    VISUAL = visual-editor;
   };
 
 }
