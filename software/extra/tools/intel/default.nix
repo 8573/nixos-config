30fce9e2d6d89861474edf15749537080f582b68
c74d <8573dd@gmail.com>
2016-11-25 09:26:34 +0000

    Fix stray semicolon in extra-Intel-tools sw-module

--- software/extra/tools/intel/default.nix
+++ software/extra/tools/intel/default.nix
@@ -2,7 +2,7 @@
   id = "intel";
   desc = "tools for Intel systems";
   default = {config, parent, ...}:
-    config.c74d-params.hardware.main-CPU-mfr == "Intel";
+    config.c74d-params.hardware.main-CPU-mfr == "Intel"
     && parent.enable;
   sw = p: with p; [
     powertop
