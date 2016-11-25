344a7a688485882195efb4420fbf93a3f29dc04c
c74d <8573dd@gmail.com>
2016-11-25 09:47:03 +0000

    Env vars: always reference programs by store path

--- sections/environment/variables/personal/default.nix
+++ sections/environment/variables/personal/default.nix
@@ -1,23 +1,24 @@
 { config, lib, pkgs, ... }: let
 
-  c74d-pkgs = config.lib.c74d.pkgs;
+  inherit (config.lib.c74d.pkgs.c74d)
+    vim-pager vim-try-x;
 
   visual-editor =
     if config.services.xserver.enable then
-      "vim-try-x-nofork"
+      "${vim-try-x}/bin/vim-try-x-nofork"
     else
-      "vim";
+      "${config.programs.vim.package}/bin/vim";
 
 in {
 
   environment.variables = lib.mkIf config.c74d-params.personal {
     EDITOR = visual-editor;
 
-    PAGER = "vim-pager";
+    PAGER = "${vim-pager}/bin/vim-pager";
 
-    MANPAGER = "vim-manpager";
+    MANPAGER = "${vim-pager}/bin/vim-manpager";
 
-    VIMPAGER_VIM = "${c74d-pkgs.c74d.vim-try-x}/bin/vim-try-x";
+    VIMPAGER_VIM = "${vim-try-x}/bin/vim-try-x";
 
     VISUAL = visual-editor;
   };
