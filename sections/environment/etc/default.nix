18ff7e8184812fa4f33a91d0deddfa4cdb646111
c74d <8573dd@gmail.com>
2017-03-25 22:18:20 +0000

    Save some c74d-params to a file in /etc

--- sections/environment/etc/default.nix
+++ sections/environment/etc/default.nix
@@ -5,4 +5,10 @@
     ./vim
   ];
 
+  environment.etc."c74d/NixOS/c74d-params.json".text =
+    builtins.toJSON
+      (lib.filterAttrs
+        (_: v: !(lib.isAttrs v))
+        config.c74d-params);
+
 }
