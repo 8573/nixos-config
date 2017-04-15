b3f73dfe1f31c1da26914ba849d15f6a321a972b
c74d <8573dd@gmail.com>
2017-04-15 18:30:11 +0000

    Finally drop users.nix from configuration.nix
    No longer include `./secret/users.nix` in the imports of
    `configuration.nix`, now that none of my installations locates its
    `users.nix` file there.

--- configuration.nix
+++ configuration.nix
@@ -10,8 +10,6 @@
     # ./installations/local should be a symlink to the directory for the
     # local system's Nixos installation.
     ./installations/local/configuration.nix
-
-    ./secret/users.nix
   ];
 
 }
