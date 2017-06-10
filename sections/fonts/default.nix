f63b60009dfc0b42326853176e73dca14c33fb69
c74d <8573dd@gmail.com>
2017-06-10 20:49:32 +0000

    (WIP) Control Fontconfig defaults

--- sections/fonts/default.nix
+++ sections/fonts/default.nix
@@ -16,7 +16,47 @@
       ];
     };
 
-    includeUserConf = false;
+    #includeUserConf = false;
+
+    localConf = let
+      overridenda = {
+        sans-serif = [
+          "Arial"
+          "Helvetica"
+          "Tahoma"
+          "Verdana"
+        ];
+        serif = [
+          "Times"
+          "Times New Roman"
+        ];
+        monospace = [
+          "Courier New"
+        ];
+      };
+      override = new: old: ''
+        <match>
+          <test name='family'>
+            <string>${old}</string>
+          </test>
+          <edit name='family' mode='assign' binding='strong'>
+            <string>${new}</string>
+          </edit>
+        </match>
+      '';
+      overrideList = list: new: map (override new) list;
+      overrides = lib.concatLists [
+        (overrideList overridenda.sans-serif "Noto Sans")
+        (overrideList overridenda.serif "Noto Serif")
+        (overrideList overridenda.monospace "DejaVu Mono")
+      ];
+    in ''
+      <?xml version='1.0' encoding='UTF-8'?>
+      <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+      <fontconfig>
+        ${lib.concatStrings overrides}
+      </fontconfig>
+    '';
   };
 
   fonts.fonts = lib.mkIf config.c74d-params.X11.enable (with pkgs; [
