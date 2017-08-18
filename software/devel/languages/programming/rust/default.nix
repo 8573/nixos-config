345a9247c0e58b75ab48bd228a19117fc9ce689c
c74d <8573dd@gmail.com>
2017-08-18 22:36:49 +0000

    Pollute global environment w/ Rust toolchain/tools

--- software/devel/languages/programming/rust/default.nix
+++ software/devel/languages/programming/rust/default.nix
@@ -1,9 +1,9 @@
 {
   id = "rust";
   desc = "software for working with the programming language Rust";
-  modules = [
-    ./global
-  ];
+  # And so I debase myself for convenience's sake
+  # What sacrifice of poor purity we wretches make
+  global = true;
   sw = p: with p; ([
     (rustChannelOf {
       channel = "1.19.0";
