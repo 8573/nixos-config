b6e00a27d89b1af7d9e526907a58aaa8baca6b0c
c74d <8573dd@gmail.com>
2017-09-30 22:09:18 +0000

    Enable PulseAudio by default for X11 systems

--- sections/hardware/default.nix
+++ sections/hardware/default.nix
@@ -2,6 +2,11 @@
 
   hardware.enableAllFirmware = config.c74d-params.enable-all-firmware;
 
+  hardware.pulseaudio = {
+    enable = lib.mkDefault
+      (!config.environment.noXlibs && !config.c74d-params.minimal);
+  };
+
   hardware.sane = {
     enable = lib.elem config.c74d-params.installation-type [
       "desktop"
