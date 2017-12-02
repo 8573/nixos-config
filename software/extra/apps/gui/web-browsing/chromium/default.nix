6370970ea2b711a576f047f107fd8f41211bb931
c74d <8573dd@gmail.com>
2017-12-02 20:03:54 +0000

    Chromium: Disable default browser check
    Wrap Chromium in a shell script passing it the command-line argument
    `--no-default-browser-check`, telling it not to complain that it's not
    the default Web browser.
    
    I would let it be the default browser, but the only notion of "default
    browser" my desktop-environment-less system has, as far as I am aware,
    is the environment variable `BROWSER`, about which Chromium doesn't
    seem to care.

--- software/extra/apps/gui/web-browsing/chromium/default.nix
+++ software/extra/apps/gui/web-browsing/chromium/default.nix
@@ -2,6 +2,10 @@
   id = "chromium";
   desc = "the Web browser Chromium";
   sw = p: with p; [
-    chromium
+    (writeScriptBin "chromium" ''
+      #!/bin/sh
+      exec '${chromium}/bin/chromium-browser' \
+        --no-default-browser-check
+    '')
   ];
 }
