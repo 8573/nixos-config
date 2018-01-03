5dee8f26d33b5d665742dbebc46611f97dd6af2b
c74d <8573dd@gmail.com>
2018-01-03 07:08:22 +0000

    Software: Wrap chromium-browser, not just chromium

--- software/extra/apps/gui/web-browsing/chromium/default.nix
+++ software/extra/apps/gui/web-browsing/chromium/default.nix
@@ -1,11 +1,14 @@
 {
   id = "chromium";
   desc = "the Web browser Chromium";
-  sw = p: with p; [
-    (writeScriptBin "chromium" ''
+  sw = p: with p; let
+    script = ''
       #!/bin/sh
       exec '${chromium}/bin/chromium-browser' \
         --no-default-browser-check
-    '')
+    '';
+  in [
+    (writeScriptBin "chromium" script)
+    (writeScriptBin "chromium-browser" script)
   ];
 }
