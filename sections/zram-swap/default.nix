8a0ead78ca6b8e570199e51d88b06e5a087c252e
c74d <8573dd@gmail.com>
2018-01-03 03:13:59 +0000

    zRAM: Enable manually, not by NixOS module
    Enable and configure zRAM via udev directly, rather than via the NixOS
    configuration module for it, because that configuration module
    interacts poorly with the NixOS hardened profile.

--- sections/zram-swap/default.nix
+++ sections/zram-swap/default.nix
@@ -3,10 +3,35 @@
   inherit (config.lib.c74d)
     mk-if-non-minimal;
 
+  mkswap = "${pkgs.utillinux}/sbin/mkswap";
+  nproc = "${pkgs.coreutils}/bin/nproc";
+  sed = "${pkgs.gnused}/bin/sed";
+  sh = "${pkgs.bash}/bin/sh";
+  swapon = "${pkgs.utillinux}/sbin/swapon";
+
 in {
 
   zramSwap = {
-    enable = mk-if-non-minimal true;
+    enable = false;
   };
+
+  boot.kernelModules = mk-if-non-minimal ["zram"];
+
+  # [2017-01-03] Derived from eternaleye's configuration.
+  #
+  # [2017-01-03] eternaleye's comment on the merits of setting zRAM capacity
+  # to the full RAM capacity, as he does, was "*shrug*", yet I trust him
+  # enough to do likewise despite some misgivings about edge cases. (What if
+  # zRAM fills the RAM and needs to swap? Might it try to swap into itself?)
+  #
+  # [2017-01-03] eternaleye's comment on the merits of running `swapon` here
+  # rather than putting the swap device in `fstab` (as NixOS's `swapDevices`
+  # configuration option would) was that the ordering between udev and `fstab`
+  # is insufficiently guaranteed.
+  services.udev.extraRules = mk-if-non-minimal ''
+    SUBSYSTEM=="block", KERNEL=="zram*", ACTION=="add", IMPORT{program}="${sh} -c 'echo NCPUS=$(${nproc})'", ATTR{comp_algorithm}="lz4", ATTR{max_comp_streams}="$env{NCPUS}"
+    SUBSYSTEM=="block", KERNEL=="zram0", ACTION=="add", IMPORT{program}="${sed} -n -e '/^MemTotal:/!d; s/^MemTotal:\s*/ZRAMSIZE=/; s/ kB/K/p' /proc/meminfo", ATTR{disksize}="$env{ZRAMSIZE}"
+    SUBSYSTEM=="block", KERNEL=="zram0", ACTION=="add", RUN+="${mkswap} /dev/%k", RUN+="${swapon} -p 30000 /dev/%k"
+  '';
 
 }
