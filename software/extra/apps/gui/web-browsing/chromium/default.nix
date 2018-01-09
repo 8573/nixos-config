67931e71a559b6052659309fb1d0d632b089efda
c74d <8573dd@gmail.com>
2018-01-09 07:58:25 +0000

    Software: Chromium: Add a security flag

--- software/extra/apps/gui/web-browsing/chromium/default.nix
+++ software/extra/apps/gui/web-browsing/chromium/default.nix
@@ -1,11 +1,28 @@
 {
   id = "chromium";
   desc = "the Web browser Chromium";
-  sw = p: with p; let
+  sw = p: let
+    inherit (p)
+      chromium lib writeScriptBin;
+
+    version = (lib.splitString "." (lib.getVersion chromium));
+
+    major-version = lib.toInt (lib.head version);
+
+    args = [
+      ["--no-default-browser-check"]
+      (lib.optional (major-version == 63)
+        # Enable this flag as a mitigation of the Spectre/Meltdown
+        # vulnerabilities. It sounds as though the flag is unavailable before
+        # and superseded after major version 63, at least for this purpose.
+        "--site-per-process")
+    ];
+
     script = ''
       #!/bin/sh
       exec '${chromium}/bin/chromium-browser' \
-        --no-default-browser-check "$@"
+        ${lib.concatStringsSep " " (lib.concatLists args)} \
+        "$@"
     '';
   in [
     (writeScriptBin "chromium" script)
