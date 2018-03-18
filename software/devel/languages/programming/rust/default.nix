02eafbf75845af78cdc5dd1f40d0eeb8aa9578e5
c74d <8573dd@gmail.com>
2018-03-18 23:03:40 +0000

    Software: Update Rust to 1.24.1
    Update my Rust toolchain installation from version 1.22.1 to version
    1.24.1 to escape a jobserver bug:
    <https://github.com/rust-lang/rust/issues/46981>.
    
    Version 1.24.1 comes with rustfmt, so this patch uninstalls the Nix
    package for rustfmt.

--- software/devel/languages/programming/rust/default.nix
+++ software/devel/languages/programming/rust/default.nix
@@ -6,10 +6,8 @@
   ];
   sw = p: with p; ([
     (rustChannelOf {
-      # The last version of 2017, as of 2017-12-30.
-      channel = "1.22.1";
+      channel = "1.24.1";
     }).rust
-    rustfmt
     rustracer
   ]);
 }
