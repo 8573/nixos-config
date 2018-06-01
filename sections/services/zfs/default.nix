c9ebdfa27deef28ed6b5461771fbae4574a4ec2d
c74d <8573dd@gmail.com>
2018-06-01 23:49:04 +0000

    ZFS: Enable automatic scrubbing

--- sections/services/zfs/default.nix
+++ sections/services/zfs/default.nix
@@ -1,6 +1,20 @@
-{ config, lib, options, pkgs, ... }: {
+{ config, lib, options, pkgs, ... }: let
+
+  inherit (config.c74d-params.location.target)
+    timezone;
+
+in {
 
   services.zfs = {
+    autoScrub = {
+      enable = config.c74d-params.usually-up;
+
+      # Scrub twice per year, on the first Thursdays of May and November, at
+      # 02:00 local time. (The default is to scrub every Sunday at 02:00
+      # system time.)
+      interval = "Thu *-05,11-01..07 02:00 ${timezone}";
+    };
+
     autoSnapshot = {
       enable = config.c74d-params.usually-up;
       flags = "${options.services.zfs.autoSnapshot.flags.default} --utc";
