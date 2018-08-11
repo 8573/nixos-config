9c53dc39bdd145785d89be9c752dac337dfe63fa
c74d <8573dd@gmail.com>
2018-08-11 05:33:29 +0000

    Document software module option inheritance rules

--- software/default.nix
+++ software/default.nix
@@ -1,10 +1,22 @@
 {
   id = "software";
+
   desc = "user software";
+
+  # `default` is *not* inherited by sub-modules. However, the state of being
+  # enabled in the installation configuration *is* inherited by sub-modules.
   default = {config, ...}:
     !config.c74d-params.minimal;
+
+  # `global` is inherited by sub-modules.
   global = true;
+
+  # `ignore-broken-pkgs` is inherited by sub-modules.
   ignore-broken-pkgs = false;
+
+  # `filter-outputs` is *not* inherited by sub-modules; it defaults to `true`.
+  filter-outputs = true;
+
   modules = [
     ./basic
     ./extra
