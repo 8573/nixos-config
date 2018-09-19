1e0717e3892e325012e388c3a894f24265834acc
c74d <8573dd@gmail.com>
2018-09-19 19:37:35 +0000

    Expand a software module

--- software/extra/apps/gui/entertainment/interactive/Dwarf-Fortress/default.nix
+++ software/extra/apps/gui/entertainment/interactive/Dwarf-Fortress/default.nix
@@ -2,6 +2,10 @@
   id = "Dwarf-Fortress";
   desc = "Dwarf Fortress";
   sw = p: with p.dwarf-fortress-packages; [
-    (dwarf-fortress.override { enableDFHack = true; })
+    (dwarf-fortress-full.override {
+      enableIntro = false;
+      enableTruetype = false;
+      enableFPS = true;
+    })
   ];
 }
