fdf47973bc38a5ff4869bd6edc103a50021d74ba
c74d <8573dd@gmail.com>
2018-10-04 01:48:36 +0000

    with-rust: Don't reimport Rust overlay

--- pkgs/c74d/with-rust/default.nix
+++ pkgs/c74d/with-rust/default.nix
@@ -1,14 +1,8 @@
-{ writeText, writeShellScriptBin, nix, nixpkgs-mozilla }:
+{ writeText, writeShellScriptBin, nix, clang, rustChannels, rustfmt }:
 
 writeShellScriptBin "with-rust" ''
   exec '${nix}/bin/nix-shell' \
     ${writeText "rust-env.nix" ''
-      with import <nixpkgs> {
-        overlays = [
-          (import "${nixpkgs-mozilla}/rust-overlay.nix")
-        ];
-      };
-
       stdenv.mkDerivation {
         name = "rust-env";
 
