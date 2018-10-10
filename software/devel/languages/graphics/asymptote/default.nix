5bfe83312a764ca4d4ce81e9d264cbeee1b076d7
c74d <8573dd@gmail.com>
2018-10-10 22:11:39 +0000

    Software: Disable Asymptote for brokenness

--- software/devel/languages/graphics/asymptote/default.nix
+++ software/devel/languages/graphics/asymptote/default.nix
@@ -1,6 +1,7 @@
 {
   id = "asymptote";
   desc = "software for working with the graphics language Asymptote";
+  default = false; # It's broken.
   global = true;
   sw = p: with p; [
     asymptote
