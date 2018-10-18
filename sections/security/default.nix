33ab82205baa42ba5aecef772cf1170cca6a87db
c74d <8573dd@gmail.com>
2018-10-18 04:10:29 +0000

    Insist that hardened profile allow user namespaces
    Insist, because Nix sandboxing requires them, that the hardened
    profile not disable Linux user namespaces. I had overridden this
    feature of the hardened profile's before, but a change to it has
    necessitated a different override.

--- sections/security/default.nix
+++ sections/security/default.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, ... }: {
 
   security = {
+    # [2018-10-18] Nix sandboxing requires user namespaces.
+    allowUserNamespaces = true;
+
     apparmor = {
       enable = true;
     };
