b02a93b22099393aa41c0aad0df5b52e7772e44f
c74d <8573dd@gmail.com>
2018-10-19 04:35:36 +0000

    Use NetworkManager on laptops

--- sections/networking/default.nix
+++ sections/networking/default.nix
@@ -1,4 +1,23 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }: let
+
+  enable-NetworkManager = lib.elem config.c74d-params.installation-type [
+    "laptop"
+  ];
+
+  enable-WiFi = lib.elem config.c74d-params.installation-type [
+    "desktop"
+  ];
+
+  enable-wpa_supplicant = enable-WiFi && !enable-NetworkManager;
+
+  Google-Public-DNS-nameservers = [
+    "8.8.8.8"
+    "2001:4860:4860::8888"
+    "8.8.4.4"
+    "2001:4860:4860::8844"
+  ];
+
+in {
 
   networking.dnsExtensionMechanism = true;
 
@@ -6,19 +25,17 @@
 
   networking.hostName = lib.mkDefault config.c74d-params.id5;
 
-  networking.nameservers = [
-    "8.8.8.8"
-    "2001:4860:4860::8888"
-    "8.8.4.4"
-    "2001:4860:4860::8844"
-  ];
+  networking.nameservers =
+    lib.mkIf (!enable-NetworkManager) Google-Public-DNS-nameservers;
 
-  networking.wireless = {
+  networking.networkmanager = lib.mkIf enable-NetworkManager {
+    enable = true;
+    insertNameservers = Google-Public-DNS-nameservers;
+  };
+
+  networking.wireless = lib.mkIf enable-wpa_supplicant {
     # Enables wireless support via wpa_supplicant.
-    enable = lib.elem config.c74d-params.installation-type [
-      "desktop"
-      "laptop"
-    ];
+    enable = true;
   };
 
 }
