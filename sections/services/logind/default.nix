22307b12d6ba703685c3e11063dcb15993bd9bd6
c74d <8573dd@gmail.com>
2019-03-03 07:33:35 +0000

    logind: Ignore all suspension/hibernation triggers

--- sections/services/logind/default.nix
+++ sections/services/logind/default.nix
@@ -4,6 +4,10 @@
     extraConfig = ''
       # For ZFS's sake, disable automatic lid-close/open suspend/resume.
       HandleLidSwitch=ignore
+      HandleLidSwitchDocked=ignore
+      HandleLidSwitchExternalPower=ignore
+      HandleSuspendKey=ignore
+      HandleHibernateKey=ignore
       # Don't break tmux.
       KillUserProcesses=no
     '';
