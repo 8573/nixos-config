23711b713e6bce63e921467841ac5ad36198e852
c74d <8573dd@gmail.com>
2019-06-08 22:51:13 +0000

    Fetch Mozilla overlay with `fetchFromGitHub` again
    I had switched to fetching the Mozilla overlay with `fetchgit` rather
    than the preferred `fetchFromGitHub`, because the latter was failing
    was connection time-out errors whereas the former, though after a
    wait, was succeeding. Now, this problem appears to have been
    temporary, and I am resuming the use of `fetchFromGitHub`

--- sections/nixpkgs/overlays/overlays.nix
+++ sections/nixpkgs/overlays/overlays.nix
@@ -2,12 +2,9 @@ let
 
   base = import <nixpkgs> {};
 
-  nixpkgs-mozilla = base.fetchgit {
-    # [2019-06-08] `fetchFromGitHub` was timing out, whereas `fetchgit` seems
-    # to work.
-    #owner = "mozilla";
-    #repo = "nixpkgs-mozilla";
-    url = "https://github.com/mozilla/nixpkgs-mozilla.git";
+  nixpkgs-mozilla = base.fetchFromGitHub {
+    owner = "mozilla";
+    repo = "nixpkgs-mozilla";
     # This revision is dated 2019-05-09.
     rev = "33bda5d711a82a2b511262ef3be367a86ef880df";
     sha256 = "0lbb22paqsn3g0ajxzw4vj7lbn9ny2vdkp5sqm3a7wrc56a8r35b";
