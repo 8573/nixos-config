c2b0497e247c62dd193b211d5e66dcd0683ac2c4
c74d <8573dd@gmail.com>
2020-06-16 02:03:45 +0000

    Add note about allowSimultaneousMultithreading

--- sections/security/default.nix
+++ sections/security/default.nix
@@ -1,6 +1,12 @@
 { config, lib, pkgs, ... }: {
 
   security = {
+    # [2020-06-16] I notice that the hardened profile disables
+    # hyper-threading. simukis seems to approve (see #rust-offtopic), I don't
+    # notice a performance degradation, and it looks as though this setting is
+    # indeed mitigating some CPU vulnerabilities that I have, so I let it be.
+    #allowSimultaneousMultithreading = ...;
+
     # [2018-10-18] Nix sandboxing requires user namespaces.
     allowUserNamespaces = true;
 
