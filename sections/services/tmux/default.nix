ee9d61479b939d018d4af93d127376683dca8d8e
c74d <8573dd@gmail.com>
2020-06-17 20:49:42 +0000

    Personal: Re-enable the tmux service

--- sections/services/tmux/default.nix
+++ sections/services/tmux/default.nix
@@ -3,7 +3,10 @@
   services.tmux = {
     # The automatically-started tmux doesn't seem to get tmux-resurrect for
     # some reason.
-    enable = config.c74d-params.personal && false;
+    #
+    # [2020-06-17] I don't know whether that remains true, but I don't seem to
+    # use tmux-resurrect anyway, so I'm re-enabling this.
+    enable = config.c74d-params.personal;
   };
 
 }
