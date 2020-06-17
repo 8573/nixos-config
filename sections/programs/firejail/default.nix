f1bc1412b18a38fb17c0e29c0241a4f512bd9e5f
c74d <8573dd@gmail.com>
2020-06-17 20:49:16 +0000

    Firejail: Add comment

--- sections/programs/firejail/default.nix
+++ sections/programs/firejail/default.nix
@@ -15,7 +15,17 @@
         VM = false;
       }.${config.c74d-params.installation-type}
     )
-    && false # Disable for now
+    # [2019-07-27] Disable for now
+    #
+    # [2020-06-17] Looking into Firejail again, the very poor image I get
+    # through the smoke (...that wasn't meant as a pun) is that it may
+    # increase security against malicious code running inside the sandbox and
+    # potentially problematically may increase attack surface to malicious
+    # code running outside the sandbox. References are
+    # - <https://www.openwall.com/lists/oss-security/2017/01/04/1>, et seq.;
+    # - <https://github.com/netblue30/firejail/issues/3046>;
+    # - etc.
+    && false
   ) {
     enable = true;
   };
