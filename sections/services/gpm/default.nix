9fd1a0b7881148d397c9a6621b68a703a07a78fb
c74d <8573dd@gmail.com>
2020-06-17 21:32:39 +0000

    GPM (mouse in the kernel console): Disable

--- sections/services/gpm/default.nix
+++ sections/services/gpm/default.nix
@@ -1,7 +1,13 @@
 { config, lib, pkgs, ... }: {
 
   services.gpm = {
-    enable = lib.mkDefault (!config.c74d-params.minimal);
+    enable = lib.mkDefault (
+      !config.c74d-params.minimal
+      # [2020-06-17] I never use GPM, so all I get from it is the annoyance of
+      # having my keyboard input interrupted if I bump the pointing device and
+      # the potential attack surface of another service running.
+      && false
+    );
   };
 
 }
