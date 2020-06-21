79558a90100a3297992f4a84daf59e9e6206de5e
c74d <8573dd@gmail.com>
2020-06-21 07:42:55 +0000

    Update CPU microcode

--- sections/hardware/default.nix
+++ sections/hardware/default.nix
@@ -1,4 +1,27 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }: let
+
+  CPU-both = {
+    # (2020-06-20) I left the microcode un-updated for years because I
+    # disliked the thought of letting my OS mess with something so important,
+    # but apparently microcode updates are required to mitigate some CPU
+    # vulnerabilities ("MDS" and "Speculative Store Bypass"). Apparently there
+    # is a risk of the kernel hanging at boot if Intel pushes a buggy
+    # microcode update, though. If that happens, I should be able to roll
+    # back, though.
+    updateMicrocode = true;
+  };
+
+  CPU-Intel = CPU-both;
+
+  CPU-AMD = CPU-both;
+
+in {
+
+  hardware.cpu = {
+    AMD = {amd = CPU-AMD;};
+    Intel = {intel = CPU-Intel;};
+    "(virtual)" = {};
+  }.${config.c74d-params.hardware.main-CPU-mfr};
 
   hardware.enableRedistributableFirmware =
     config.c74d-params.enable-most-firmware;
