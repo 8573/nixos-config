f96e6dbfe7f001752dfcee582692176235fd29b0
c74d <8573dd@gmail.com>
2020-10-09 23:56:22 +0000

    Software: Uninstall Okular

--- software/extra/apps/gui/pdf/Okular.nix
+++ software/extra/apps/gui/pdf/Okular.nix
@@ -3,8 +3,11 @@
   desc = ''
     the heavy-weight document-viewer Okular
   '';
+  # [2020-10-09]  Okular's GUI feels sluggish (menus can take a second to
+  # open) and it doesn't seem to have implemented as much of the PDF standard
+  # as MuPDF has.  I see no need to keep its ~250 MiB around.
   default = {config, parent, ...}:
-    { desktop = true;
+    { desktop = false;
       laptop = false;
       server = false;
       VM = false; }
